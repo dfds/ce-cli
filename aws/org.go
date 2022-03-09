824c2cbd51f0248c60f2fc39b225a312e9eb2d25
abstrask <raras@dfds.com>
2022-03-09 11:24:45 +0100

    Initial get list of AWS accounts

--- aws/org.go
+++ aws/org.go
@@ -1,7 +1,12 @@
 package aws
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/organizations"
 
 	"github.com/spf13/cobra"
 )
@@ -11,5 +16,18 @@ func OrgAccountListCmd(cmd *cobra.Command, args []string) {
 }
 
 func OrgAccountList() {
-	fmt.Println("aws list-org-accounts")
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	svc := organizations.NewFromConfig(cfg)
+
+	accounts, err := svc.ListAccounts(context.TODO(), &organizations.ListAccountsInput{})
+	if err != nil {
+		log.Fatalf("unable to list accounts, %v", err)
+	}
+
+	fmt.Println(accounts.Accounts)
+
 }
